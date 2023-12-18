f60417d7c8df7725f93fbc322b771a3486b8eb61
Falaron <arthurjouve91@gmail.com>
2023-12-18 16:16:59 +0100

    voilà

--- view/vadmin.php
+++ view/vadmin.php
@@ -125,9 +125,9 @@
                                 <?php } else {
                                     if ($playerKey == "ID") continue; ?>
                                     <div>
-                                        <div class="input-field col s12">
+                                        <div>
                                             <label for="<?php echo $playerKey ?>"><?php echo $playerKey ?></label>
-                                            <textarea id="textarea1" class="materialize-textarea" name="<?php echo $playerKey; ?>"><?php echo $playerInfo; ?></textarea>
+                                            <textarea id="textarea1" name="<?php echo $playerKey; ?>"><?php echo $playerInfo; ?></textarea>
                                         </div>
                                     </div>
                                 <?php }
@@ -135,7 +135,6 @@
                         </div>
                         <button type="submit">SAVE</button>
                       </form>
-
                       </div>
                     </div>
                   </li>
@@ -144,14 +143,124 @@
             </li>
           <?php } ?>
         </ul>
-        <div>
-          <button type="submit" data-target="modal-add-project">SAVE</button>
-        </div>
+        <!-- Player Project Add -->
+        <div id="modal-add-project">
+          <div>
+            <section>
+              <h2>Add your own player!</h2>
+              <form action="config/add_player.php" method="POST" enctype="multipart/form-data">
+              <div>
+                <?php foreach($dataPlayer[0] as $playerKey => $playerInfo){
+                  if (isImage($playerInfo)) { ?>
+                    <div>
+                        <div>
+                            <label for="<?php echo $playerKey ?>"><?php echo $playerKey ?></label>
+                            <input type="file" name="<?php echo $playerKey; ?>">
+                        </div>
+                    </div>
+                <?php } else {
+                      if ($playerKey == "ID") continue; ?>
+                      <div>
+                        <div>
+                          <label for="name"><?php echo $playerKey ?></label>
+                          <textarea id="textarea1" name="<?php echo $playerKey; ?>"></textarea>
+                        </div>
+                      </div>
+                <?php } 
+                } ?>
+              </div>
+              <div>
+                <button type="submit">SAVE</button>
+              </div>
+              </form>
+            </section>
+          </div>
       </div>
       
       <?php //require('assets/parts/project-add.php'); ?>
     </div>
   </body>
+
+  <script>
+    function addGame() {
+      const gamesContainer = document.getElementById('games-container');
+      const newGameInput = document.createElement('div');
+      newGameInput.className = 'game-input';
+      const newGameNumber = gamesContainer.children.length + 1;
+      newGameInput.innerHTML = `
+        <label for="game${newGameNumber}">Game ${newGameNumber}:</label>
+        <input type="text" class="game" name="games['name'][]" required>
+        <input type="text" class="rank" name="games['rank'][]" >
+        <button type="button" class="remove-btn" onclick="removeGame(this)">Remove</button>
+      `;
+      gamesContainer.appendChild(newGameInput);
+
+      // Enable the remove button in the existing game input
+      const existingGameInput = gamesContainer.querySelector('.game-input');
+      if (existingGameInput) {
+        const removeBtn = existingGameInput.querySelector('.remove-btn');
+        removeBtn.removeAttribute('disabled');
+      }
+    }
+
+    function removeGame(button) {
+      const gamesContainer = document.getElementById('games-container');
+      const gameInput = button.parentNode;
+      gamesContainer.removeChild(gameInput);
+
+      // Disable the remove button if there is only one game
+      const remainingGameInputs = gamesContainer.children.length;
+      if (remainingGameInputs === 1) {
+        const removeBtn = gamesContainer.querySelector('.remove-btn');
+        removeBtn.setAttribute('disabled', 'disabled');
+      }
+    }
+
+    function addSkill() {
+      const skillsContainer = document.getElementById('skills-container');
+      const newSkillInput = document.createElement('div');
+      newSkillInput.className = 'skill-input';
+      const newSkillNumber = skillsContainer.children.length + 1;
+      newSkillInput.innerHTML = `
+        <label for="skill${newSkillNumber}">Skill ${newSkillNumber}:</label>
+        <input type="text" class="skill" name="skills[]" required>
+        <button type="button" class="remove-btn" onclick="removeSkill(this)">Remove</button>
+      `;
+      skillsContainer.appendChild(newSkillInput);
+
+      // Enable the remove button in the existing skill input
+      const existingSkillInput = skillsContainer.querySelector('.skill-input');
+      if (existingSkillInput) {
+        const removeBtn = existingSkillInput.querySelector('.remove-btn');
+        removeBtn.removeAttribute('disabled');
+      }
+    }
+
+    function removeSkill(button) {
+      const skillsContainer = document.getElementById('skills-container');
+      const skillInput = button.parentNode;
+      skillsContainer.removeChild(skillInput);
+
+      // Disable the remove button if there is only one skill
+      const remainingSkillInputs = skillsContainer.children.length;
+      if (remainingSkillInputs === 1) {
+        const removeBtn = skillsContainer.querySelector('.remove-btn');
+        removeBtn.setAttribute('disabled', 'disabled');
+      }
+    }
+
+    function copyContent() {
+      // Get the content from the textarea
+      var bioTextarea = document.getElementById('player-bio');
+      var bioHiddenInput = document.getElementById('hidden-player-bio');
+
+      // Set the value of the hidden input to the textarea content
+      bioHiddenInput.value = bioTextarea.value;
+    }
+
+    // Trigger copyContent when the textarea loses focus
+    document.getElementById('player-bio').addEventListener('blur', copyContent);
+  </script>
 
   <script type="text/javascript"> 
   function openBox(evt, category) {
