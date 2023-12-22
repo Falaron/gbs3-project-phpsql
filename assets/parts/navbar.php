09423240d0ee0d19da3fca7fb8def05952f77df2
Falaron <arthurjouve91@gmail.com>
2023-12-22 13:54:39 +0100

    modifications

--- assets/parts/navbar.php
+++ assets/parts/navbar.php
@@ -13,28 +13,21 @@
   ?>
 
 <!-- NAVBAR ON PC -->
-<a div class="header">
-    <nav class="navbar">
-        <a href="index.php" class="logo">Team Rooster Pro</a>
+<nav class="navbar">
+    <a href="index.php" class="logo">Team Rooster Pro</a>
 
-        <?php if(isset($_SESSION['login']) && !$_SESSION['login']) {?>
-          <a href='login.php'>Login</a>
-          <a href='signup.php'>Signup</a>
-        <?php }
-        elseif (isset($_SESSION['login']) && $_SESSION['login'] == true && $_SESSION['role'] == 1) { ?>
-          <a href='admin.php'>Admin Panel</a>
-          <?php }
-        if (isset($_SESSION['login']) && $_SESSION['login'] == true) { ?>
-          <a href='forgot-password.php'>Forgot Password</a>
-          <a href='config/logout.php'>Logout</a>
-          <?php } ?>
-        
+    <?php if(isset($_SESSION['login']) && !$_SESSION['login']) {?>
+      <a href='login.php'>Login</a>
+      <a href='signup.php'>Signup</a>
+    <?php }
+    elseif (isset($_SESSION['login']) && $_SESSION['login'] == true && $_SESSION['role'] == 1) { ?>
+      <a href='admin.php'>Admin Panel</a>
+      <?php }
+    if (isset($_SESSION['login']) && $_SESSION['login'] == true) { ?>
+      <a href='forgot-password.php'>Forgot Password</a>
+      <a href='config/logout.php'>Logout</a>
+      <?php } ?>
+    
 
-        <span> <?php echo (isset($_SESSION['login']) && $_SESSION['login'] == true) ? "Hello ".$hello : 'You are logged out'; ?> </span></li>
-    </nav> 
-      <ul>
-      <?php
-      //foreach($data as $dataPlayer){ ?>
-        <!-- <a href="esport-profile.php?id=<?php //echo $dataPlayer['ID'];?>"><?php// echo $dataPlayer['player_name'];?> Profile</a> -->
-  </div>
+    <span><?php echo (isset($_SESSION['login']) && $_SESSION['login'] == true) ? "Hello ".$hello : 'You are logged out'; ?> </span>
 </nav>
--- view/vindex.php
+++ view/vindex.php
@@ -4,13 +4,7 @@
     <title>Team Roster Pro</title>
     <meta name="description" content="Login to lead your Esport team now.">
     <meta charset="utf-8">
-    <!-- Let browser know website is optimized for mobile -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- Disable the cache to avoid versioning problems -->
-    <meta http-equiv="cache-control" content="max-age=0" />
-    <meta http-equiv="cache-control" content="no-cache" />
-    <meta http-equiv="expires" content="0" />
   </head>
 
   <body>
