cc839a7a4947ba0bd7c230e758c6b0b57471b619
Falaron <arthurjouve91@gmail.com>
2023-12-22 13:59:25 +0100

    PUSH!!!

--- view/vadmin.php
+++ view/vadmin.php
@@ -7,10 +7,6 @@
     <!-- Let browser know website is optimized for mobile -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- Disable the cache to avoid versioning problems -->
-    <meta http-equiv="cache-control" content="max-age=0" />
-    <meta http-equiv="cache-control" content="no-cache" />
-    <meta http-equiv="expires" content="0" />
   </head>
 
   <body>
@@ -21,9 +17,11 @@
     <h1>ADMIN PANEL</h1>
 
     <!-- Tab links -->
-    <div class="tab">
-      <button class="tablinks" onclick="openBox(event, 'MANAGERS')">MANAGERS</button>
-      <button class="tablinks" onclick="openBox(event, 'PLAYERS')">PLAYERS</button>
+    <div id="header">
+      <div class="tab">
+        <button class="tablinks" onclick="openBox(event, 'MANAGERS')">MANAGERS</button>
+        <button class="tablinks" onclick="openBox(event, 'PLAYERS')">PLAYERS</button>
+      </div>
     </div>
 
     <!-- Manager Tab content -->
